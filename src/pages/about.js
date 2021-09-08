89b4f75cfa8497129bdccea855aaea377b722c44
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-09-07 23:19:24 -0300

    📦 refactor: add about contact info

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,10 +4,27 @@ export async function About() {
 
   $div.innerHTML = `
       <address>
-        <h1>Sobre</h1>
-        <p>Contact us by phone (11 2345-6789),
-        e-mail (vanillajs@javascript.com) 
-        </p>
+      <h3 align="center">Meus contatos:</h3>
+
+      <p align="center">
+      
+      <a title="Instagram" href="https://www.instagram.com/gleison_gbass/">
+          <img class="instagram" alt="Instagram Logo" >
+      </a>
+      
+      <a title="Linkedin" href="https://www.linkedin.com/in/gleison-ribeiro-a65257119/">
+          <img class="linkedin" alt="Linkedin Logo" width="40px" height="40px"  >
+      </a>
+      
+      <a title="Youtube" href="https://www.youtube.com/channel/UCxZ1pK5ExBbS6VS0x5YMGfA?view_as=subscriber">
+          <img class="youtube" alt="Youtube Logo">
+      </a>
+      
+      <a title="Medium" href="https://gleisonsubzerokz.medium.com/">
+          <img class="medium" alt="Medium Logo">
+      </a>
+      
+      </p>
       </address>  
     `;
   return $div;
